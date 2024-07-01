43b106b66e4a0e7679205facd200d0c5767aac4a
Büşra Yılmaz <busra_ylmz1993@hotmail.com>
2024-07-02 00:09:36 +0300

    Models and View cshtml pages Added

--- ASPNETCoreMVCApp/Controllers/ProductController.cs
+++ ASPNETCoreMVCApp/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPNETCoreMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,45 @@ namespace ASPNETCoreMVCApp.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult GetProducts()
+
+        #region ViewResult
+        //render view file (.cshtml) as response 
+        //base ViewResult_ViewStart.cshtml
+        //public ViewResult GetProducts()
+        //{
+        //    ViewResult result = View();
+        //    return result;
+        //}
+
+        #endregion
+
+        #region PartialViewResult
+        //public PartialViewResult GetProducts()
+        //{
+            //render view file (.cshtml) as response 
+            //use Ajax teq client base
+            //PartialViewResult result = PartialView();
+            //return result;
+        //}
+
+        #endregion
+
+        #region JsonResult
+        public JsonResult GetProducts()
         {
-            return View();
+            JsonResult jsonResult = Json(new Product
+            {
+                Id = 1,
+                ProductName = "pen",
+                Quantity = 2
+            }) ;
+            return jsonResult;
         }
+
+        #endregion
+        //public IActionResult GetProducts()
+        //{
+        //    return View();
+        //}
     }
 }
