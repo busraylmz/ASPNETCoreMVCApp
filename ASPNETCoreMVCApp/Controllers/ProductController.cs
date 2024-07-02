dd7c04733d43e7cf3304ce1299359f85d7742977
byilmaz <byilmaz@havelsan.com.tr>
2024-07-02 12:59:42 +0300

    ActionTypes Added

--- ASPNETCoreMVCApp/Controllers/ProductController.cs
+++ ASPNETCoreMVCApp/Controllers/ProductController.cs
@@ -10,44 +10,70 @@ namespace ASPNETCoreMVCApp.Controllers
     public class ProductController : Controller
     {
 
+
         #region ViewResult
-        //render view file (.cshtml) as response 
-        //base ViewResult_ViewStart.cshtml
+
         //public ViewResult GetProducts()
         //{
         //    ViewResult result = View();
         //    return result;
         //}
 
+
         #endregion
 
         #region PartialViewResult
         //public PartialViewResult GetProducts()
         //{
-            //render view file (.cshtml) as response 
-            //use Ajax teq client base
-            //PartialViewResult result = PartialView();
-            //return result;
+        //    PartialViewResult partialViewResult = PartialView();
+        //    return partialViewResult;
         //}
-
         #endregion
 
-        #region JsonResult
-        public JsonResult GetProducts()
-        {
-            JsonResult jsonResult = Json(new Product
-            {
-                Id = 1,
-                ProductName = "pen",
-                Quantity = 2
-            }) ;
-            return jsonResult;
-        }
-
-        #endregion
-        //public IActionResult GetProducts()
+        #region JSONResult
+        //public JsonResult GetProducts()
         //{
-        //    return View();
+
+        //    JsonResult jsonResult = Json(new Product { Id = 5, Name = "Terlik", Quantity = 2 });
+        //    return jsonResult;
         //}
+        #endregion
+
+        #region EmptyResult
+        //public EmptyResult GetProducts()
+        //{
+        //    return new EmptyResult();
+        //}
+        #endregion
+
+        #region ContentResult
+        //public ContentResult GetProducts()
+        //{
+        //    ContentResult result = Content("busra....");
+
+        //    return result;
+        //}
+        #endregion
+
+        #region ActionResult
+        //public ActionResult GetProducts()
+        //{
+        //    ActionResult result = Content("busra....");
+
+        //    return result;
+        //}
+        #endregion
+
+        #region IActionResult
+        public IActionResult GetProducts()
+        {
+            IActionResult result = Json("busra....");
+
+            return result;
+        }
+        #endregion
     }
+
+
+
 }
